7cae82ef520ca43d4453c9a07ba11a4eed4fe97e
YanzheShi <yanzheshi@outlook.com>
2018-05-09 21:37:45 +0800

    Interface default method and static method

--- src/com/github/yanzheshi/jdk8/NewInterface.java
+++ src/com/github/yanzheshi/jdk8/NewInterface.java
@@ -40,7 +40,71 @@ class A implements NewInterface {
 class B implements NewInterface {
     @Override
     public String defaultMethod() {
-        return "B";
+        return "B default method";
+    }
+}
+
+/**
+ * 继承接口
+ * 可以覆盖父父接口的默认方法
+ *
+ */
+interface Interface2 extends NewInterface {
+    /**
+     * 继承接口覆盖了父接口的默认方法
+     * 默认方法
+     * 实现类可以不覆盖默认方法，使用接口中默认的实现方式
+     * @return string
+     */
+    @Override
+    default String defaultMethod() {
+        return "Interface2 default method";
+    }
+
+}
+
+interface Interface3 extends NewInterface {
+
+}
+
+class C implements Interface2 {
+
+}
+
+/**
+ * 测试类
+ */
+class NewInterfaceTest {
+    public static void main(String[] args) {
+
+        //接口静态方法
+
+        //只能通过接口名调用接口静态方法
+        System.out.println(NewInterface.staticMethod());
+
+        //但是无法通过接口实现类调用接口静态方法
+//        System.out.println(A.staticMethod());  nonsupport
+
+        //与类不同， 接口静态方法不会被继承
+//        System.out.println(Interface2.staticMethod());  nonsupport
+
+
+        A a = new A();
+        B b = new B();
+        C c = new C();
+
+        //接口默认方法测试
+
+        //默认方法可以不用实现类实现，此时调用默认方法实现
+        System.out.println(a.defaultMethod());
+
+        //实现类可以覆盖接口默认方法
+        System.out.println(b.defaultMethod());
+
+        //子接口也可以覆盖默认方法， 调用对是子接口覆盖后对默认方法
+        System.out.println(c.defaultMethod());
+
+
     }
 }
 
--- src/com/github/yanzheshi/jdk8/Test.java
+++ src/com/github/yanzheshi/jdk8/Test.java
@@ -5,6 +5,18 @@ package com.github.yanzheshi.jdk8;
  */
 public class Test {
     public static void main(String[] args) {
+        System.out.println(BB.method());
         System.out.println();
     }
+}
+
+
+class AA {
+    public static String method() {
+        return "AA";
+    }
+}
+
+class BB extends AA{
+
 }
