b619da4709a3635b37a38ab5ac25d99100dcd805
YanzheShi <yanzheshi@outlook.com>
2018-05-15 17:41:16 +0800

    jdk 7 Objects deepEquals

--- src/com/github/yanzheshi/jdk7/ObjectsUtil.java
+++ src/com/github/yanzheshi/jdk7/ObjectsUtil.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  */
 public class ObjectsUtil {
     public static void main(String[] args) {
-        ObjectsUtil.nullValue();
+        ObjectsUtil.deepEquals();
     }
 
     /**
@@ -38,7 +38,7 @@ public class ObjectsUtil {
      * 更加简洁与直观
      */
     public static void newEqual() {
-        String a = "a";
+        String a = null;
         String b = "b";
 
         if (Objects.equals(a, "a")) {
@@ -49,6 +49,7 @@ public class ObjectsUtil {
         if (Objects.equals(a, b)) {
             System.out.println("equal");
         }
+
     }
 
     /**
@@ -76,6 +77,22 @@ public class ObjectsUtil {
         //如果为空， 用另一个值代替
         notNull = Objects.requireNonNullElse(a, "b");
         System.out.println(notNull);
+
+    }
+
+    /**
+     * deepEquals, 如果是集合说数组，比较各个元素是否相等
+     * 否则于Object.equals相同
+     */
+    public static void deepEquals() {
+        int a[] = {1, 2, 3};
+        int b[] = {1, 2, 3};
+
+        //直接判断， 返回false
+        System.out.println(Objects.equals(a, b));
+
+        //deepEquals返回true
+        System.out.println(Objects.deepEquals(a, b));
     }
 
 }
