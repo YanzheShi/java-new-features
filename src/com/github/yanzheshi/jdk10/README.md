394a2f1103d91f0a32be53648604ccf374c2cf10
YanzheShi <yanzheshi@outlook.com>
2018-05-21 23:19:17 +0800

    JDK 10 局部变量类型推断

---
+++ src/com/github/yanzheshi/jdk10/README.md
@@ -0,0 +1,6 @@
+#JDK 10 新特性
+
+---
+##参考资料
+
+[JDK 10 新特性](https://www.oschina.net/translate/109-new-features-in-jdk-10)
---
+++ src/com/github/yanzheshi/jdk10/localvariable/LocalVariable.java
@@ -0,0 +1,13 @@
+package com.github.yanzheshi.jdk10.localvariable;
+
+/**
+ * 局部变量类型推导
+ * 可以使用var声明变量或引用
+ */
+public class LocalVariable {
+    public static void main(String[] args) {
+        var a = 1;
+
+        System.out.println(a);
+    }
+}
